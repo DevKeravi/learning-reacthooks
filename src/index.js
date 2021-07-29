9cf41aee26b774f81cb9e138cbed9c7631edd8d2
cmkrosp <cmkrosp@gmail.com>
2021-07-30 06:49:20 +0900

    made useScroll and commit before rest

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
+const useScroll = () => {
+  const [state,setState] = useState({
+    x: 0,
+    y: 0
+  });
+  const onScroll = () => {
+    setState({y: window.scrollY, x: window.scrollX});
+  };
+  useEffect(() => {
+      window.addEventListener("scroll",onScroll);
+      return () => window.removeEventListener("scroll",onScroll);
+  },[]);
+  return state;
+}
+
 const App = () => {
+  const {y} = useScroll();
   return (
-    <div className="App">
-      <div>Hi</div>
+    <div className="App" style={{height: "1000vh"}}>
+      <h1 style={{positon: "fixed", color: y > 10 ? "red" : "blue" }}>Bye</h1>
     </div>
   );
 };
