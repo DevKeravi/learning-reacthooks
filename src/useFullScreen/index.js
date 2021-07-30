75f9abcab6a02206c51732be76a3790ecf3c796b
Keravi <cmkrosp@gmail.com>
2021-07-31 01:45:14 +0900

    Add useFullScreen

---
+++ src/useFullScreen/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const triggerFull = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      if (callback && typeof callback === "function") {
+        callback(true);
+      }
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (callback && typeof callback === "function") {
+      callback(false);
+    }
+  };
+  return { element, triggerFull, exitFull };
+};
+
+const App = () => {
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are FullScreen" : "Exiting FullScreen");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <div ref={element}>
+        <img src="https://img.hankyung.com/photo/201906/03.19980925.1.jpg" />
+        <button onClick={exitFull}>Exit fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
